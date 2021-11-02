422aa61cc9ef8685e387ae0e1b151eb0baeac7da
Marcin Maćkowiak <marcin@mmackowiak.com>
2021-11-02 12:37:40 +0100

    Updated cmake, swiftlint and swiftformat

---
+++ Formula/swiftformat@0.48.17.rb
@@ -0,0 +1,30 @@
+class SwiftformatAT04817 < Formula
+  desc "Formatting tool for reformatting Swift code"
+  homepage "https://github.com/nicklockwood/SwiftFormat"
+  url "https://github.com/nicklockwood/SwiftFormat/archive/0.48.17.tar.gz"
+  sha256 "b0c81730a4b19e76057e5f9b781d124d5ca697bb80cde7638ea3c6fc8b8d2ed0"
+  license "MIT"
+  head "https://github.com/nicklockwood/SwiftFormat.git", branch: "master"
+
+  depends_on xcode: ["10.1", :build]
+  depends_on :macos
+
+  def install
+    xcodebuild "-arch", Hardware::CPU.arch,
+        "-project", "SwiftFormat.xcodeproj",
+        "-scheme", "SwiftFormat (Command Line Tool)",
+        "-configuration", "Release",
+        "CODE_SIGN_IDENTITY=",
+        "SYMROOT=build", "OBJROOT=build"
+    bin.install "build/Release/swiftformat"
+  end
+
+  test do
+    (testpath/"potato.swift").write <<~EOS
+      struct Potato {
+        let baked: Bool
+      }
+    EOS
+    system "#{bin}/swiftformat", "#{testpath}/potato.swift"
+  end
+end
