e6f52c4777cb35e4dda03cb1e8faf9e360792855
Marcin Maćkowiak <marcin@mmackowiak.com>
2022-01-25 14:32:44 +0100

    Added xcbeautify@0.11.0

---
+++ Formula/xcbeautify@0.11.0.rb
@@ -0,0 +1,24 @@
+class XcbeautifyAT0110 < Formula
+  desc "Little beautifier tool for xcodebuild"
+  homepage "https://github.com/thii/xcbeautify"
+  url "https://github.com/thii/xcbeautify.git",
+      tag:      "0.11.0",
+      revision: "b14660cc03d96889d2144265fbca471ea0076e40"
+  license "MIT"
+  head "https://github.com/thii/xcbeautify.git", branch: "master"
+
+  depends_on xcode: ["11.4", :build]
+
+  def install
+    system "swift", "build", "--disable-sandbox", "--configuration", "release"
+    bin.install ".build/release/xcbeautify"
+  end
+
+  test do
+    log = "CompileStoryboard /Users/admin/MyApp/MyApp/Main.storyboard (in target: MyApp)"
+    assert_match "[\u{1B}[36mMyApp\u{1B}[0m] \u{1B}[1mCompiling\u{1B}[0m Main.storyboard",
+      pipe_output("#{bin}/xcbeautify", log).chomp
+    assert_match version.to_s,
+      shell_output("#{bin}/xcbeautify --version").chomp
+  end
+end
