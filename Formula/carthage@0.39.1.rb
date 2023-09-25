94340f4f4c3bdac976676c53213009900f8be6da
Marcin Maćkowiak <marcin@mmackowiak.com>
2023-09-25 15:12:28 +0200

    Updated carthage

---
+++ Formula/carthage@0.39.1.rb
@@ -0,0 +1,24 @@
+class CarthageAT0391 < Formula
+  desc "Decentralized dependency manager for Cocoa"
+  homepage "https://github.com/Carthage/Carthage"
+  url "https://github.com/Carthage/Carthage.git",
+      tag:      "0.39.1",
+      revision: "edf085cc652b9be8270720c21d2ca1b9fb12ff94"
+  license "MIT"
+  head "https://github.com/Carthage/Carthage.git", branch: "master"
+
+  depends_on xcode: ["10.0", :build]
+  depends_on :macos
+
+  def install
+    system "make", "prefix_install", "PREFIX=#{prefix}"
+    bash_completion.install "Source/Scripts/carthage-bash-completion" => "carthage"
+    zsh_completion.install "Source/Scripts/carthage-zsh-completion" => "_carthage"
+    fish_completion.install "Source/Scripts/carthage-fish-completion" => "carthage.fish"
+  end
+
+  test do
+    (testpath/"Cartfile").write 'github "jspahrsummers/xcconfigs"'
+    system bin/"carthage", "update"
+  end
+end
